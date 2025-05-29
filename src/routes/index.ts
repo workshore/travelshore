4ef7d2e2a62bbf06dbfbacce602c0624e66f2cdc
hari-wio <hari.kishore@workshore.io>
2025-05-29 17:21:43 +0530

    Disabled authentication

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -37,7 +37,7 @@ export const app = () => {
         });
     });
 
-    new WFRoute(USER_PATHS.appRoute).withMiddleware(userMiddleware, "USER", "allow", {
+    new WFRoute(USER_PATHS.appRoute).withMiddleware(userMiddleware, "GUEST", "allow", {
         onError: () => {
             navigate(USER_PATHS.landing);
         }
